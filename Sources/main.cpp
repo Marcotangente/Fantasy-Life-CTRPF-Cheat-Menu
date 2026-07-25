dbc91e1246c977104812619afaf523e68da2eb3a
venkeyz <vennnkeyw@gmail.com>
2026-07-25 08:25:52 +0100

    test money

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -7,7 +7,7 @@
 namespace CTRPluginFramework
 {
     // This patch the NFC disabling the touchscreen when scanning an amiibo, which prevents ctrpf to be used
-    static void    ToggleTouchscreenForceOn(void)
+    static void ToggleTouchscreenForceOn(void)
     {
         static u32 original = 0;
         static u32 *patchAddress = nullptr;
@@ -25,18 +25,18 @@ namespace CTRPluginFramework
             0xE1D400D4, 0xE3510003, 0x159F0034, 0x1A000003
         };
 
-        Result  res;
-        Handle  processHandle;
-        s64     textTotalSize = 0;
-        s64     startAddress = 0;
-        u32 *   found;
+        Handle processHandle;
+        s64 textTotalSize = 0;
+        s64 startAddress = 0;
+        u32 *found;
 
         if (R_FAILED(svcOpenProcess(&processHandle, 16)))
             return;
 
         svcGetProcessInfo(&textTotalSize, processHandle, 0x10002);
         svcGetProcessInfo(&startAddress, processHandle, 0x10005);
-        if(R_FAILED(svcMapProcessMemoryEx(CUR_PROCESS_HANDLE, 0x14000000, processHandle, (u32)startAddress, textTotalSize)))
+
+        if (R_FAILED(svcMapProcessMemoryEx(CUR_PROCESS_HANDLE, 0x14000000, processHandle, (u32)startAddress, textTotalSize)))
             goto exit;
 
         found = (u32 *)Utils::Search<u32>(0x14000000, (u32)textTotalSize, pattern);
@@ -44,62 +44,71 @@ namespace CTRPluginFramework
         if (found != nullptr)
         {
             original = found[13];
-            patchAddress = (u32 *)PA_FROM_VA((found + 13));
+            patchAddress = (u32 *)PA_FROM_VA(found + 13);
             found[13] = 0xE1A00000;
         }
 
         svcUnmapProcessMemoryEx(CUR_PROCESS_HANDLE, 0x14000000, textTotalSize);
-exit:
+
+    exit:
         svcCloseHandle(processHandle);
     }
 
-    // This function is called before main and before the game starts
-    // Useful to do code edits safely
-    void    PatchProcess(FwkSettings &settings)
+    // Called before the game starts
+    void PatchProcess(FwkSettings &settings)
     {
         ToggleTouchscreenForceOn();
     }
 
-    // This function is called when the process exits
-    // Useful to save settings, undo patchs or clean up things
-    void    OnProcessExit(void)
+    // Called when the game exits
+    void OnProcessExit(void)
     {
         ToggleTouchscreenForceOn();
     }
 
-    void    InitMenu(PluginMenu &menu)
+    // Money editor
+    void MoneyEditor(MenuEntry *entry)
     {
-        // Create your entries here, or elsewhere
-        // You can create your entries whenever/wherever you feel like it
-        
-        // Example entry
-        /*menu += new MenuEntry("Test", nullptr, [](MenuEntry *entry)
-        {
-            std::string body("What's the answer ?\n");
+        u32 money;
 
-            body += std::to_string(42);
+        // Read current value
+        Process::Read32(0x08A47BE8, money);
 
-            MessageBox("UA", body)();
-        });*/
+        Keyboard kb("Money");
+        kb.IsHexadecimal(true);
+
+        if (kb.Open(money) != 0)
+            return;
+
+        // Write new value
+        Process::Write32(0x08A47BE8, money);
     }
 
-    int     main(void)
+    void InitMenu(PluginMenu &menu)
     {
-        PluginMenu *menu = new PluginMenu("Action Replay", 0, 8, 0,
-                                            "A blank template plugin.\nGives you access to the ActionReplay and others tools.");
+        menu += new MenuEntry("Money", MoneyEditor);
+    }
 
-        // Synnchronize the menu with frame event
+    int main(void)
+    {
+        PluginMenu *menu = new PluginMenu(
+            "Action Replay",
+            0,
+            8,
+            0,
+            "A blank template plugin.\nGives you access to the ActionReplay and others tools."
+        );
+
+        // Synchronize the menu with frame events
         menu->SynchronizeWithFrame(true);
 
-        // Init our menu entries & folders
+        // Add menu entries
         InitMenu(*menu);
 
-        // Launch menu and mainloop
+        // Run the plugin
         menu->Run();
 
         delete menu;
-
-        // Exit plugin
-        return (0);
+        return 0;
     }
 }
